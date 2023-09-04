bf3567a4037f4a547670c5a48b2024511e882016
Lawrence <lawrencehome21@gmail.com>
2023-09-04 10:18:10 -0700

    playing audio

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let audio = new Audio("");
+
+const playTune = (key) => {
+    audio.play(); // playing audio
+}
+
 pianoKeys.forEach(key => {
     // calling the playTune function with passing data-key value as an argument
     key.addEventListener("click", () => playTune(key.dataset.key));
