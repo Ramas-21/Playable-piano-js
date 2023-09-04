01b00e37d352b7f2a3d4ec5de609c0a2e07b6d5f
Lawrence <lawrencehome21@gmail.com>
2023-09-04 10:21:26 -0700

    passed audio src based on the key pressed

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-let audio = new Audio("");
+let audio = new Audio("tunes/a.wav"); // by default, audio src is "a" tune
 
 const playTune = (key) => {
+    audio.src = `tunes/${key}.wav`; // passing audio src based on the key pressed
     audio.play(); // playing audio
 }
 
