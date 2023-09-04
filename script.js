d2e941f6676ca7a0981b4e9fc2c725500944bea8
Lawrence <lawrencehome21@gmail.com>
2023-09-04 10:28:26 -0700

    getting clicked key element

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ let audio = new Audio("tunes/a.wav"); // by default, audio src is "a" tune
 const playTune = (key) => {
     audio.src = `tunes/${key}.wav`; // passing audio src based on the key pressed
     audio.play(); // playing audio
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
 }
 
 pianoKeys.forEach(key => {
     // calling the playTune function with passing data-key value as an argument
     key.addEventListener("click", () => playTune(key.dataset.key));
 });
+
+const pressedKey = (e) => {
+    playTune(e.key);
+}
+
+document.addEventListener("keydown", pressedKey);
