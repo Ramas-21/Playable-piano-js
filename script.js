eb3bf65997d5cf9e065919bef79008978bbee32d
Lawrence <lawrencehome21@gmail.com>
2023-09-04 10:35:36 -0700

    added active class to the clicked key element & removed active class after 150 ms from the clicked key element

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const playTune = (key) => {
     audio.play(); // playing audio
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
+    clickedKey.classList.add("active"); //adding active class to the clicked key element
+    setTimeout(() => { // removing active class after 150 ms from the clicked key element
+        clickedKey.classList.remove("active");
+    }, 150);
 }
 
 pianoKeys.forEach(key => {
