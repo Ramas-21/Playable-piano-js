4d240faff09cbd090fed1d5239347891114130c7
Lawrence <lawrencehome21@gmail.com>
2023-09-04 09:57:56 -0700

    called the playTune function with passing data-key value as an argument

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-pianoKeys.forEach(key)
+pianoKeys.forEach(key => {
+    // calling the playTune function with passing data-key value as an argument
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});
